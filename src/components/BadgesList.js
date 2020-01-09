e8e4e095fe2ef47ba50724169cca25d369157e0d
Manuel Romero <manuelromero@blacksip.com>
2020-01-09 08:19:51 -0500

    se crearon llamadas a un api

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,46 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import './styles/BadgesList.css';
+import "./styles/BadgesList.css";
+import { Link } from "react-router-dom";
 
 class BadgesListItem extends React.Component {
-  render() {
-    return (
-      <div className="BadgesListItem">
-        <img
-          className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl}
-          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
-        />
+	render() {
+		return (
+			<div className="BadgesListItem">
+				<img
+					className="BadgesListItem__avatar"
+					src={this.props.badge.avatarUrl}
+					alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+				/>
 
-        <div>
-          <strong>
-            {this.props.badge.firstName} {this.props.badge.lastName}
-          </strong>
-          <br />@{this.props.badge.twitter}
-          <br />
-          {this.props.badge.jobTitle}
-        </div>
-      </div>
-    );
-  }
+				<div>
+					<strong>
+						{this.props.badge.firstName} {this.props.badge.lastName}
+					</strong>
+					<br />@{this.props.badge.twitter}
+					<br />
+					{this.props.badge.jobTitle}
+				</div>
+			</div>
+		);
+	}
 }
 
 class BadgesList extends React.Component {
-  render() {
-    return (
-      <div className="BadgesList">
-        <ul className="list-unstyled">
-          {this.props.badges.map(badge => {
-            return (
-              <li key={badge.id}>
-                <BadgesListItem badge={badge} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		if (this.props.badges.length === 0) {
+			return (
+				<div>
+					<h3>No encontramos ningun badge</h3>
+					<Link className="btn btn-primary" to="/badges/new">
+						Create new badges
+					</Link>
+				</div>
+			);
+		}
+
+		return (
+			<div className="BadgesList">
+				<ul className="list-unstyled">
+					{this.props.badges.map(badge => {
+						return (
+							<li key={badge.id}>
+								<BadgesListItem badge={badge} />
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default BadgesList;
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,73 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/Badges.css';
-import confLogo from '../images/badge-header.svg';
-import BadgesList from '../components/BadgesList';
+import "./styles/Badges.css";
+import confLogo from "../images/badge-header.svg";
+import BadgesList from "../components/BadgesList";
+import api from "../api";
 
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-        firstName: 'Freda',
-        lastName: 'Grady',
-        email: 'Leann_Berge@gmail.com',
-        jobTitle: 'Legacy Brand Director',
-        twitter: 'FredaGrady22221-7573',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-      },
-      {
-        id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-        firstName: 'Major',
-        lastName: 'Rodriguez',
-        email: 'Ilene66@hotmail.com',
-        jobTitle: 'Human Research Architect',
-        twitter: 'MajorRodriguez61545',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-      },
-      {
-        id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-        firstName: 'Daphney',
-        lastName: 'Torphy',
-        email: 'Ron61@hotmail.com',
-        jobTitle: 'National Markets Officer',
-        twitter: 'DaphneyTorphy96105',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-      },
-    ],
-  };
+	constructor(props) {
+		super();
+		console.log("1, soy el constructor");
+		this.state = {
+			data: [],
+			loading: true,
+			error: null,
+		};
+	}
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="Badges">
-          <div className="Badges__hero">
-            <div className="Badges__container">
-              <img
-                className="Badges_conf-logo"
-                src={confLogo}
-                alt="Conf Logo"
-              />
-            </div>
-          </div>
-        </div>
+	componentDidMount() {
+		console.log("3, soy el didmount");
 
-        <div className="Badges__container">
-          <div className="Badges__buttons">
-            <Link to="/badges/new" className="btn btn-primary">
-              New Badge
-            </Link>
-          </div>
+		this.fetchData();
+	}
 
-          <BadgesList badges={this.state.data} />
-        </div>
-      </React.Fragment>
-    );
-  }
+	fetchData = async () => {
+		this.setState({ loading: true, error: null });
+		try {
+			const data = await api.badges.list();
+			this.setState({ loading: false, data: data });
+			console.log("data", data);
+		} catch (error) {
+			console.log("error", error);
+			this.setState({ loading: false, error: error });
+			console.log("desde el esatDOOO", this.state.error);
+		}
+	};
+	componentDidUpdate(prevProps, prevState) {
+		console.log("5 - componentDidUpdate");
+		console.log("prevProps", prevProps);
+		console.log("prevState", prevState);
+		console.log("Props", this.props);
+		console.log("State", this.state);
+	}
+
+	componentWillUnmount() {
+		console.log("6 - willUnmount");
+		clearTimeout(this.timeoutID);
+	}
+	render() {
+		console.log("2, render");
+		if (this.state.loading === true) {
+			return "Loading...";
+		}
+
+		if (this.state.error) {
+			return `Error: ${this.state.error.menssage}`;
+		}
+
+		return (
+			<React.Fragment>
+				<div className="Badges">
+					<div className="Badges__hero">
+						<div className="Badges__container">
+							<img
+								className="Badges_conf-logo"
+								src={confLogo}
+								alt="Conf Logo"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className="Badges__container">
+					<div className="Badges__buttons">
+						<Link to="/badges/new" className="btn btn-primary">
+							New Badge
+						</Link>
+					</div>
+
+					<BadgesList badges={this.state.data} />
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Badges;
